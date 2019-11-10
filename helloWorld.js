5fc71bbf730ccd0e9833c9cfe9356b169956f188
Marta Matos <marta.ra.matos@gmail.com>
2019-11-10 15:08:11 +0100

    adding not that easy xD

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,13 @@ const helloWorld = function() {
   return 'Hello world!';
 };
 
+const heyThere = function() {
+  return 'Not that easy :)';
+};
+
+
 console.log(helloWorld());
+
+console.log(heyThere());
 
 console.log("Almost there!");
